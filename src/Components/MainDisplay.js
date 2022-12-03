502066acba62efd073adea5407e0706817c025b0
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-03 23:21:46 +0300

    Test adding API info

--- src/Components/MainDisplay.js
+++ src/Components/MainDisplay.js
@@ -1,15 +1,22 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
+// const mySecret = process.env.REACT_APP_NEWS_API_KEY 
+// const gNewsKey = process.env.REACT_APP_GNEWS_API_KEY 
 
 function MainDisplay(){
-    // This was to confirm that the Proxy is working! 
 
-    // useEffect(() => {
-    //     fetch('http://localhost:3000')
+    // function getGnews(){
+    //     fetch(`https://gnews.io/api/v4/world?token=${gNewsKey}&topic=breaking-news`)
+    //     .then(r => r.json())
     //     .then(d => console.log(d))
-    //     .catch(e => console.log(e))
+    // }
 
-    // }); 
+    useEffect(() => { 
+        // fetch(`https://newsapi.org/v2/everything?q=Apple&from=2022-12-03&sortBy=popularity&apiKey=${mySecret}`)
+        // .then(r => r.json())
+        // .then(d => console.log(d.articles))
 
+        // getGnews()
+    },[])
     return(
         <>
         <h2 className="text-orange-200 pt-3 text-2xl">All Components Should be housed here and everything else will receive state as props!</h2>
@@ -21,3 +28,5 @@ export default MainDisplay;
 
 
 // Attempt push after merge
+
+// 'https://newsapi.org/v2/everything?q=Apple&from=2022-12-03&sortBy=popularity&apiKey=API_KEY'
