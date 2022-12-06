fabd9b38ef179c2024888703133254349d2efbc3
Gracelaura <gracenjuguna05@gmail.com>
2022-12-06 11:28:49 +0300

    added event handlers

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,16 +1,46 @@
-
+import React from 'react'
 
 function LoginForm() {
     
+    const[formData, setFormData] = useState({
+        username:"",
+        password:""
+    }) 
+
+    function handleLogin(e) {
+        e.preventDefault()
+        // AWAITING BACKEND
+        fetch('',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body :JSON.stringify(formData)
+        })
+        .then(resp => resp.json())
+        .then(result => {
+            console.log(result)
+        })
+        console.log(password)
+        console.log(username)
+    }
+    
+    const {username,password} = formData
+
+    function handleChange(e) {
+        e.target.name
+        setFormData({...formData, [e.target.name] : [e.target.value]})
+    }
+
     return (
-        <form>
+        <form onSubmit = {handleLogin}>
             <label>
                 Username
-               <input type="text"/> 
+               <input type="text" value = {formData.username} onChange = {(e) => handleChange}/> 
             </label>
             <label>
                 Password
-               <input type="password"/> 
+               <input type="password" value = {formData.password} onChange= {(e) => handleChange}/> 
             </label>
             <input type="submit"/> 
             
