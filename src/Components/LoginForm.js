868dfd3ca33257f259c7e513cd54639d73973688
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-06 12:26:45 +0300

    Start user authentication logic

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,20 +1,58 @@
+import { useState } from 'react'
+import { redirect } from 'react-router';
 
+function LoginForm({ onLogin }){
+    const [ username, setUsername ] = useState(""); 
+    const [ password, setPassword ] = useState("");
+    const [ error, setError ] = useState("");
 
-function LoginForm() {
-    
-    return (
-        <form>
-            <label>
-                Username
-               <input type="text"/> 
-            </label>
-            <label>
-                Password
-               <input type="password"/> 
-            </label>
-            <input type="submit"/> 
-            
+    function fetchUser(e){
+        e.preventDefault(); 
+        
+        fetch('/login', {
+            method : 'POST',
+            headers : {
+                "Content-Type" : "application/json"
+            }, 
+            body : JSON.stringify( { username , password })
+            })
+            .then(r => {
+            console.log(r)
+            if(r.ok){
+                r.json().then((user) => onLogin(user))
+                return redirect("/home")
+            }
+            else {
+                r.json().then((err) => setError(err.errors))
+            }
+            })
+  }
+
+    return(
+         <form onSubmit={fetchUser} className="grid gap-5 p-3">
+               <input type="text" 
+               className="text-black" 
+               value={username} 
+               onChange={(e) => {
+                // console.log(e.target.value)
+                setUsername(e.target.value)
+               }}
+               placeholder="Enter your username"/> 
+
+               <input type="password" 
+               className="text-black" 
+               value={password} 
+               onChange={(e) => {
+                // console.log(e.target.value)
+                setPassword(e.target.value)
+               }}
+               placeholder="Enter Password"/> 
+
+              <input type="submit"/>   
+
+               <span hidden={error === ""}>{error}</span>
         </form>
     )
-}
-export default LoginForm
+  }
+
+  export default LoginForm
--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,20 +1,55 @@
+import { useState } from 'react'
 
+function SignUpForm( { onLogin }){
+    const [ username, setUsername ] = useState(""); 
+    const [ password, setPassword ] = useState("");
+    const [ errors, setErrors ] = useState([]);
 
-function SignupForm() {
-    const [ formData, setFormData ] = useState({ username: ' ' , password: ' '})
-    return (
-        <form>
-            <label>
-                Username
-               <input type="text" value={formData.username}/> 
-            </label>
-            <label>
-                Password
-               <input type="password" value={formData.password}/> 
-            </label>
-            <input type="submit"/> 
-            
+    function userSignUp(e){
+        e.preventDefault()
+
+        fetch('/signup',{
+            method : 'POST',
+            headers : { "Content-Type" : 'application/json' },
+            body : JSON.stringify({ username, password })
+        })
+        .then(r => {
+            if(r.ok){
+                r.json().then(user => onLogin(user))
+            } else { 
+                r.json().then(e => { 
+                    setErrors(e.errors)
+                    })
+            }
+        })
+  }
+
+    return(
+         <form onSubmit={userSignUp} className="grid gap-5 p-3">
+               <input type="text" 
+               className="text-black" 
+               value={username} 
+               onChange={(e) => {
+                setUsername(e.target.value)
+               }}
+               placeholder="Enter your username"/> 
+
+               <input type="password" 
+               className="text-black" 
+               value={password} 
+               onChange={(e) => {
+                setPassword(e.target.value)
+               }}
+               placeholder="Enter Password"/> 
+              <button onClick={userSignUp}>Sign Up</button>
+
+              <div hidden={errors.length <= 0 }>
+                  {errors.map((err) => {
+                    return <h4 key={errors.indexOf(err)}>{err}</h4>
+                  })}
+              </div>
         </form>
     )
-}
-export default SignupForm
+  }
+
+  export default SignUpForm;
